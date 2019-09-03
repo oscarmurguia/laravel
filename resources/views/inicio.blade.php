4ae88e1478ff3cfbb7825398d4d267f537821ee3
Oscar Murguia <oscarindex@gmail.com>
2019-09-03 13:51:59 -0400

    cambioderuta
    Se realizo el cambio de la ruta de welcome a inicio

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Esta es una prueba de cambios en Git Hub</h1>
+  </body>
+</html>
