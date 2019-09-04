1d0dfd527838796fe5fa8a5c2e491c82f4003ae7
Oscar Murguia <oscarindex@gmail.com>
2019-09-04 12:05:40 -0400

    actualizando
    actualice las vistas

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,4 +4,9 @@
 @endsection
 @section('contenido')
   <h1>Hola a la pagina de inicio</h1>
+  <ul>
+    <li>Cosas para comprar de la tienda:</li>
+    <li>Leche</li>
+    <li>Huevos</li>
+  </ul>
 @endsection
