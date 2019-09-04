20edf7f96381fef13566c7d28e68ab1011360a42
Oscar Murguia <oscarindex@gmail.com>
2019-09-04 11:54:10 -0400

    cambios
    cambios en el pull

--- app/Http/Controllers/PortafolioController.php
+++ app/Http/Controllers/PortafolioController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PortafolioController extends Controller
 {
     public function index(){
-      return view ('inicio');
+      return view ('contactos'); //Estes es un ejemplo 04-09-2019
     }
 }
